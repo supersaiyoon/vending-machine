e2f0f15a0f102639a1716b682ee7098dc3f95874
Brian Yoon <byoonie@gmail.com>
2022-10-24 21:08:18 -0700

    Created JSON files

---
+++ main.java
@@ -0,0 +1,41 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.Date;
+
+class VendingMachineFile {
+
+}
+
+class VendingMachine {
+    String location;
+    
+    void processOrder() {}
+    void addItem() {}
+    void removeItem() {}
+    void changePrice() {}
+    void getSaleData() {}
+    void verifyExpiration() {}
+    void verifyRecall() {}
+    void verifyStock() {}
+}
+
+class Customer {
+
+}
+
+class Transaction {
+    
+}
+
+class Error {
+
+}
+
+class Main {
+    public static void main(String arg[]) {
+        JSONObject vendingMachine1 = new JSONObject();
+    }
+}
