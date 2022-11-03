4ab1fd386b5527e81a282c655f0e9ed159e200b1
Brian <byoonie@gmail.com>
2022-11-02 18:48:29 -0700

    Created VendingMachineFile class (#5)

--- main.java
+++ main.java
@@ -3,16 +3,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.JSONTokener;
+import java.io.InputStream;
 
 import java.util.Date;
 
-class VendingMachineFile {
-
-}
 
 class VendingMachine {
     String location;
@@ -39,53 +35,81 @@ class Error {
 
 }
 
-class Main {
-    public static void main(String arg[]) {
-        // JSON parser object to parse read file
-        JSONParser jsonParser = new JSONParser();
-        
-        try (FileReader reader = new FileReader("VendingMachineFile.json")) {
-            // Read JSON file
-            Object obj = jsonParser.parse(reader);
+class VendingMachineFile {
+    public String slotNum;       // Must store value within class for functions to work.
+    public JSONObject tokenObj;  // Access nested fields through this.
 
-            JSONObject vendingMachineList = (JSONObject) obj;
-            String prettyJson = vendingMachineList.toString(2);
-            System.out.println(prettyJson);
-            
-            // Grab value from nested field in JSON
-            JSONObject testMachine = (JSONObject) vendingMachineList.get("vendingMachineTest");
-            JSONObject testMachineAddress = (JSONObject) testMachine.get("address");
-            String streetName = (String) testMachineAddress.get("streetName");
-            System.out.println(streetName);
+    public VendingMachineFile() {
+        String fileName = "VendingMachineFile.json";
+        InputStream is = Main.class.getResourceAsStream(fileName);
 
-            // Accessing product name in slot A1
-            JSONObject testMachineA1Slot = (JSONObject) testMachine.get("slot");
-            JSONObject slot = (JSONObject) testMachineA1Slot.get("A1");
-            String productName = (String) slot.get("productName");
-            System.out.println(productName);
-
-            slot.put("productName","Pringles");
-            productName = (String) slot.get("productName");
-            
-            // Write JSON file
-            try (FileWriter file = new FileWriter("VendingMachineFile.json")) {
-                file.write(vendingMachineList.toString(2));
-                file.flush();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
+        if (is == null) {
+            throw new NullPointerException("Cannot find JSON file.");
         }
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
+
+        JSONTokener tokener = new JSONTokener(is);
+        tokenObj = new JSONObject(tokener);
+    }
+
+    void setSlot(String slotNum) {
+        this.slotNum = slotNum;
+    }
+
+    public static String getAddress(JSONObject tokenObj) {
+        // Return full address.
+        // streetName, city, country
+        String address = "";
+    
+        return address;
+    }
+
+    int getSlotQty() {
+        JSONObject vendingMachineObj = tokenObj.getJSONObject("vendingMachine_Sacramento");
+        JSONObject slotObj = vendingMachineObj.getJSONObject("slot");
+        JSONObject slotName = slotObj.getJSONObject(slotNum);
+
+        return slotName.getInt("quantity");
+    }
+
+    double getSlotPrice() {
+        double price = 0;
+
+        return price;
+    }
+
+    String getSlotProductName() {
+
+    }
+
+    String getExpDate() {
+
+    }
+}
+
+
+class Main {
+    public static void main(String args[]) {
+
+        VendingMachineFile testFile = new VendingMachineFile();
+        
+        System.out.println(testFile.getSlotQty());
+       
+
+        // Leap frog to get to the specific nested level you want.
+        // These should become functions.
+
+
+        //System.out.println(addressObj.getString("streetName"));
+
+        //addressObj.put("streetName", "789 Main St");
+
+        // Include the below in setter functions.
+        /*try (FileWriter file = new FileWriter("VendingMachineFile.json")) {
+            file.write(tokenObj.toString(2));
+            file.flush();
         }
         catch (IOException e) {
             e.printStackTrace();
-        }
-        catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        //JSONObject testObj = new JSONObject();
+        }*/
     }
 }
